b15480afff532426b679f3b46b619e7d8946ad50
Omid Arzani <omid.arzani@gmx.net>
2019-09-24 20:01:36 +0200

    pathWithEntriesAdded

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -23,19 +23,35 @@ const Main = styled.main`
   padding-bottom: 25px;
 `;
 
-function Home() {
+function Home({ history, location }) {
+  const params = new URLSearchParams(location.search);
   const [filters, setFilters] = React.useState({
-    categories: "sushi"
+    categories: params.get("categories") || "",
+    distance: params.get("distance") || "",
+    rating: params.get("price") || ""
   });
   function handleFilterChange(name, value) {
+    // Create a copy of filter objects
     const newFilters = { ...filters };
+
     if (value) {
       newFilters[name] = value;
     } else {
       delete newFilters[name];
     }
-    //Frage.
+
     setFilters(newFilters);
+
+    let searchString = "/?";
+    const newFiltersAsArray = Object.entries(newFilters);
+    for (const [filterName, filterValue] of newFiltersAsArray) {
+      searchString += `${filterName}=${filterValue} &`;
+    }
+    let searchStringFinal = searchString.slice(0, -1);
+    history.push(searchStringFinal);
+    // history.push(
+    //   `/?categories=${newFilters.categories}&distance=${newFilters.distance}&price=${newFilters.price}`
+    // );
   }
   return (
     <Appdiv>
